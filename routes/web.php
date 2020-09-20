769309deccad43e89c53a3b5a07498a39b946b2c
Julia Lanu <julia@erecode.nl>
2020-09-20 15:39:47 +0200

    two new views and connected route to start with ;)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/wanteditems', function () {
+    return view('wanteditems');
+});
+
+Route::get('/trade', function () {
+    return view('trade');
+});
