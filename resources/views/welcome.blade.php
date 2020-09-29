c07c80958defb531e5f49099c68cf93e8bd7964b
Julia Lanu <julia@erecode.nl>
2020-09-29 20:35:34 +0200

    it works

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CrossWide
+                    CrossWide yeet
                 </div>
 
                 <div class="links">
