85bdf25f064883716aaca5bbd62bdc2a8252ac82
Julia Lanu <julia@erecode.nl>
2020-11-09 14:33:40 +0100

    RAW Crud added in routes - testing

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,42 @@ Route::get('/trade', function () {
     return view('trade');
 });
 
+//INSERT - CREATE
+
+Route::get('/insert', function(){
+  DB::insert('insert into items(title, body) values(?,?)', ['Lucky cat', 'Rare recipe']);
+
+});
+
+//READ DATA - select
+
+Route::get('/read', function(){
+  $results = DB::select('select *  from items where id = ?', [1]);
+
+  // foreach($results as $item){
+  //
+  //   return $item->title;
+  //
+  // }
+
+});
+
+// UPDATE DATA
+
+Route::get('/update', function(){
+  $updated = DB::update('update items set title = "Bamboo stool" where id = ?', [1]);
+
+  return $updated;
+});
+
+// DELETE DATA
+
+Route::get('/delete', function(){
+  $deleted = DB::delete('delete from items where id = ?', [1]);
+
+  return $deleted;
+});
+
 
 
 //WITH CONTROLLER
